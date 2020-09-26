56073317fc37050587dda5d63ef5f70c99ee27ed
Ryan Buckley <ryanbuckley@gmail.com>
2020-09-26 01:34:52 -0400

    create api route from express to fetch into react as a test

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+router.get("/testAPI", function(req, res, next) {
+  res.send('this is a response sent back from Express server at localhost:9000/testAPI');
 });
 
 module.exports = router;

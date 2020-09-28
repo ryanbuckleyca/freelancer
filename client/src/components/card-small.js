434ecf6fc862a9c398c559f21a1c4dacd4f0eb1a
Ryan Buckley <ryanbuckley@gmail.com>
2020-09-28 14:01:05 -0400

    contain img in card-sm

--- client/src/components/card-small.js
+++ client/src/components/card-small.js
@@ -7,7 +7,7 @@ class CardSmall extends Component {
       <div className="col-12 col-md-6 py-3 px-4 p-md-4 mb-4 mb-md-3 card-sm">
         <div className="row">
           <div className="col-5 card-img d-flex align-items-center">
-            <img src={this.props.img} alt='info' />
+            <img className="w-100" src={this.props.img} alt='info' />
           </div>
           <div className="col-7 card-text">
             <strong className="d-block mb-2">{this.props.cardTitle}</strong>

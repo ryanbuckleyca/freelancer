d8078cbbf95e6ad59267839b39ccaa13df39d032
Ryan Buckley <ryanbuckley@gmail.com>
2020-10-04 15:26:44 -0400

    initialize CRUD for user profile

---
+++ api/authenticate.js
@@ -0,0 +1,27 @@
+// Auth0 authentication middleware
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+const port = process.env.API_PORT || 9000;
+const appOrigin = process.env.APP_ORIGIN;
+const audience = process.env.AUTH0_AUDIENCE;
+const issuer = process.env.AUTH0_ISSUER;
+
+if (!issuer || !audience) {
+  throw new Error("Please make sure that .env is in place and populated");
+}
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${issuer}.well-known/jwks.json`,
+  }),
+
+  audience: audience,
+  issuer: issuer,
+  algorithms: ["RS256"],
+});
+
+module.exports = checkJwt;
--- api/models/user.js
+++ api/models/user.js
@@ -1,23 +1,20 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const db = require('./index')
+// const Household = require('./household')
+
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    auth0_id: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  const User = sequelize.define('User', {
+    auth0_id: DataTypes.STRING,
+    name: DataTypes.STRING,
+    number: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {});
+  // User.associate = function(models) {
+  //   User.belongsTo(
+  //     models.Household,
+  //     { foreignKey: 'householdId' }
+  //   )
+  // };
   return User;
 };

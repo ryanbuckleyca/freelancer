2bf75996fc3e37f16ce3966cfadae62d29c10f36
Ryan Buckley <ryanbuckley@gmail.com>
2020-10-04 20:09:16 -0400

    try eliminate catchall

--- api/server.js
+++ api/server.js
@@ -95,9 +95,9 @@ app.get("/test", (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match the ones above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/../client/build/index.html'));
+// });
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/../client/build')));

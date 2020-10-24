3ad6850519953446f1fe28f609251a6b3a1b4a7e
Ryan Buckley <ryanbuckley@gmail.com>
2020-10-24 11:36:00 -0400

    remove sub-gb, add buttons, format styles

--- src/components/card-form-topside-profile.js
+++ src/components/card-form-topside-profile.js
@@ -26,7 +26,12 @@ class CardFormTopsideProfile extends Component {
       <div className="card-form-img">
         <div className="card-form-avatar">
           <img src={this.props.picture} alt="avatar" />
-          <a href="#" className="btn btn-dark btn-sm" onClick={this.cloudinaryWidget}>edit</a>
+          <div className="profile-buttons">
+          <a className="btn btn-secondary" onClick={this.cloudinaryWidget}>edit image</a>
+          <a className="btn btn-secondary">payment info</a>
+          <a className="btn btn-secondary">my clients</a>
+          <a className="btn btn-danger">delete account</a>
+          </div>
         </div>
         <button id="card-form-btn-side" className="btn btn-primary" onClick={this.props.handleSubmit}>
           Update profile
